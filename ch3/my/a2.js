ad54b617383f5c593f353da2e1f534c8f8ce9840
Luke <tale987654321@hotmail.com>
2018-07-07 11:07:05 +0800

    fix(*) ch3

---
+++ ch3/my/a2.js
@@ -0,0 +1,18 @@
+const isEven = num => {
+    if (num == 0) {
+        return true;
+    } else if (num == 1) {
+        return false;
+    } else if (num<=-1) {
+        return isEven(num+2);
+    } else {
+        return isEven(num-2);
+    }
+};
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
---
+++ ch3/my/a3.js
@@ -0,0 +1,23 @@
+const countBs = (str) => {
+    let count = 0;
+    for (let i=0; i<str.length; i++) {
+       if(str[i]==='B') {
+           count = count + 1 ;
+       }
+    }
+    return count;
+};
+
+const countChar = (str, char)=> {
+    let count = 0;
+    for (let i=0; i<str.length; i++) {
+       if(str[i]===char) {
+           count = count + 1 ;
+       }
+    }
+    return count;
+};
+
+console.log(countBs("BBC"));
+
+console.log(countChar("kakkerlak", "k"));
